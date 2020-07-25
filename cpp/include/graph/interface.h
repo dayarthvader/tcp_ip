bc38ad4854c68e57138456172001cc0de7c8aaa8
Dayarthvader <28380956+dayarthvader@users.noreply.github.com>
2020-07-25 23:17:18 +0530

    added spdlog and googletest external dependencies

--- cpp/include/graph/interface.h
+++ cpp/include/graph/interface.h
@@ -3,15 +3,18 @@
 #define CPP_INCLUDE_GRAPH_INTERFACE_H_
 #include <string>
 
-namespace graph {
+#include "util/tcp_ip_errors.h"
+using util_ns::tcp_ip_errors;
+
+namespace graph_ns {
 class Interface {
  public:
   explicit Interface(const std::string& name);  //  why explicit? Go figure!
-  Connect(int link_id);                         // Todo After StatusOr
+  tcp_ip_errors Connect(int link_id);           // Todo After StatusOr
  private:
   std::string name_{""};
   bool connected_{false};
   int link_id{-1};
 };
-}  //  namespace graph
+}  // namespace graph_ns
 #endif  // CPP_INCLUDE_GRAPH_INTERFACE_H_
